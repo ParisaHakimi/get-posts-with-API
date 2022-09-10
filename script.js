0e0ea2d03da26f8387f2368b3061602c1602947b
Parisa Sohrabi <parisahs@gmail.com>
2022-09-10 18:17:03 -0400

    ability to get each post

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+const wrapper = document.querySelector(".wrapper");
+
 const getAllPosts = async () => {
   let html = "";
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts = await response.json();
-posts.forEach((post) => {
-console.log(post)
+  posts.forEach((post) => {
+    // console.log(post);
 
     html += `<div class="post">
           <h2>${post.title}</h2>
           <p>${post.body}</p>
           </div>`;
   });
-  document.querySelector('.wrapper').innerHTML=html;
+  wrapper.innerHTML = html;
 };
+
+document.querySelector(".wrapper").addEventListener("click", async (e) => {
+  if (e.target.className === "post") {
+    wrapper.innerHTML = `<div class="spinner"></div>`;
+    const postId = e.target.dataset.id;
+    console.log(postId);
+    const apiUrl = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    const response = await fetch(apiUrl);
+    console.log(response);
+    const singlePost = await response.json();
+    wrapper.innerHTML = `<div class="single-post">
+    <h1>${singlePost.title}</h1>
+    <p>${singlePost.body}</p>
+    <button class="show-all">show all posts</button></div>`;
+  }
+  // console.log(wrapper);
+});
 getAllPosts();
